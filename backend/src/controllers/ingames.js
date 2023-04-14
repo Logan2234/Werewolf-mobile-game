1ec4baad04ce2818807fa742aa7c1d3f35d8c49b
Jorge Luri Vañó <jorge.luri-vano@grenoble-inp.org>
2023-04-14 17:39:37 +0200

    getMessagesFromPlace

--- backend/src/controllers/ingames.js
+++ backend/src/controllers/ingames.js
@@ -20,25 +20,24 @@ module.exports = {
         // ! TODO
     },
 
-    async getMessages (req, res) {
+    async getMessagesFromPlace (req, res) {
         const username = req.login
         let {idSession} = req.params
         const userId = (await userModel.findOne({where: {username}})).id
         const userInGame = await inGameModel.findOne({where: {"idUser": userId, "idGame": parseInt(idSession)}})
         if (!userInGame) throw new CodeError(username + ' is not in game ' + idSession, status.FORBIDDEN)
-        const momentJournee = (await inGameModel.findOne({where: {"id": idSession}})).moment
-        if (moment == "J") {
-            const idLieu = await lieuModel.findOne({where: {"id": idSession, "typeLieu": "P"}})
-            if (userInGame.vie == "M") {
-                const messages = await messageModel.findAll({where: {"idLieu": idLieu}})
-                res.json({status: true, message: 'Messages of the Central Place from the beginning', messages})
-                return
-            } else {
-                const messages = await messageModel.findAll({where: {"idLieu": idLieu, "archive": false}})
-                res.json({status: true, message: 'Messages of the Central Place from the beginning', messages})
-                return
-            }
+        const idLieu = await lieuModel.findOne({where: {"id": idSession, "typeLieu": "P"}})
+        if (userInGame.vie == "M") {
+            const messages = await messageModel.findAll({where: {"idLieu": idLieu}})
+            res.json({status: true, message: 'Messages of the Central Place from the beginning', messages})
+            return
+        } else {
+            const messages = await messageModel.findAll({where: {"idLieu": idLieu, "archive": false}})
+            res.json({status: true, message: 'Messages of the Central Place from the beginning', messages})
+            return
         }
     }
+
+
 }
 
--- backend/src/routes/inGames.js
+++ backend/src/routes/inGames.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const user = require('../controllers/user.js');
-const inGames = require('../controllers/inGames.js');
+const inGames = require('../controllers/ingames.js');
+
+router.get('/game/:idGame/place', user.verificationUser, inGames.getMessagesFromPlace);
 
 
 module.exports = router

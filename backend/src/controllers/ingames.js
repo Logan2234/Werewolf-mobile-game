9a9ca462adade41b57ed782fe227f5a75702b0a9
Jorge Luri Vañó <jorge.luri-vano@grenoble-inp.org>
2023-04-14 16:09:35 +0200

    Ajout de finTimer à inGames

--- backend/src/controllers/ingames.js
+++ backend/src/controllers/ingames.js
@@ -6,6 +6,7 @@ const inGameModel = require('../models/inGames.js');
 const usersInQModel = require('../models/usersInQs.js');
 const lieuModel = require('../models/lieux.js');
 const messageModel = require('../models/messages.js');
+const urneModel = require('../models/urnes.js');
 const CodeError = require('../util/CodeError.js')
 
 const has = require('has-keys');
@@ -15,11 +16,8 @@ var timers = {}
 module.exports = {
     async startNight(idGame) {
         await inGameModel.update({"moment": "N"}, {where: {"id": idGame}})
-        // Hurne
+        const urne = urneModel.findOne({where: {"idGame": idGame}})
+        // ! TODO
     }
-}
-
-function getTimeLeft(timeout) {
-    return Math.ceil((timeout._idleStart + timeout._idleTimeout - Date.now()) / 1000);
 }
 

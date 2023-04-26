b91962ba1a782bb7db320ed5713e3ae07ceda585
Logan Willem <logan.willem@grenoble-inp.org>
2023-04-26 23:43:48 +0200

    Ajout du websocket et fonctionnalité ajoutée pour la sharesession

--- frontend/src/constants/backend.js
+++ frontend/src/constants/backend.js
@@ -1,2 +1,3 @@
 // export const BACKEND = 'https://projet-web.osc-fr1.scalingo.io';
-export const BACKEND = 'http://192.168.0.105:3000';
+export const BACKEND = 'http://192.168.112.63:3000';
+export const WEBSOCKET = 'ws://192.168.112.63:8080';
--- frontend/src/screens/PlayerView.js
+++ frontend/src/screens/PlayerView.js
@@ -1,15 +1,17 @@
-import { FlatList, StyleSheet } from 'react-native';
 import { useContext, useEffect, useState } from 'react';
-import { BACKEND } from '../constants/backend';
-import { gameViews } from '../constants/screens';
-import { CurrentGameView } from '../constants/hooks';
+import { FlatList, StyleSheet } from 'react-native';
 import SizedText from '../components/SizedText';
+import { BACKEND } from '../constants/backend';
 import { primaryColor } from '../constants/colors';
+import { CurrentGameView, TokenContext } from '../constants/hooks';
+import { gameViews } from '../constants/screens';
 
 export default function PlayerView({ idSession }) {
     const [alivePlayers, setAlivePlayers] = useState([]);
     const [deadPlayers, setDeadPlayers] = useState([]);
+    const [aliveWerewolves, setAliveWerewolves] = useState([]);
 
+    const token = useContext(TokenContext);
     const currentGameView = useContext(CurrentGameView);
 
     useEffect(() => {
@@ -40,18 +42,51 @@ export default function PlayerView({ idSession }) {
                 })
                 .catch(error => alert(error.message));
         }
+
+        function fetchUserRole() {
+            let role = null;
+            fetch(`${BACKEND}/user/status`, {
+                method: 'GET',
+                headers: {
+                    'x-access-token': token,
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(data => role = data.role)
+                .catch(error => alert(error.message));
+            return role;
+        }
+
+        function fetchAliveWerewolves() {
+            fetch(`${BACKEND}/game/${idSession}/werewolves`, {
+                method: 'GET',
+            })
+                .then(response => response.json())
+                .then(data => {
+                    let users = [];
+                    for (let user of data.werewolves)
+                        users.push({ key: user, color: 'orange' });
+                    setAliveWerewolves(users);
+                })
+
+                .catch(error => alert(error.message));
+        }
+
         if (currentGameView == gameViews.PLAYERS) {
             fetchAliveData();
             fetchDeadData();
+            if (fetchUserRole() === 'LG')
+                fetchAliveWerewolves();
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentGameView]);
 
     return (
         <FlatList
             renderItem={({ item }) => <SizedText size={20} label={item.key} style={{ color: item.color }} />}
-            data={alivePlayers.concat(deadPlayers)}
-            contentContainerStyle={styles.flatListContainer}  style={styles.flatList}/>
+            data={alivePlayers.concat(deadPlayers).concat(aliveWerewolves)}
+            contentContainerStyle={styles.flatListContainer} style={styles.flatList} />
     );
 }
 

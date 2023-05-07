fb4f54ad87df709399d8518ee19bab1b2f1af1b3
Logan Willem <logan.willem@grenoble-inp.org>
2023-05-07 16:05:03 +0200

    Vote view improved in term of usestate

--- frontend/src/screens/VoteView.js
+++ frontend/src/screens/VoteView.js
@@ -1,13 +1,14 @@
-import { View, StyleSheet } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import Title from '../components/Title';
-import { useEffect, useState, useContext } from 'react';
+import { BACKEND } from '../constants/backend';
+import { CurrentGameView, TokenContext } from '../constants/hooks';
+import { gameViews } from '../constants/screens';
 import ChoixUrne from './ChoixUrne';
 import StaticUrne from './StaticUrne';
-import { TokenContext, CurrentGameView } from '../constants/hooks';
-import { BACKEND } from '../constants/backend';
 
 
-export default function VoteView({idSession}) {
+export default function VoteView({ idSession }) {
     const [voteState, setVoteState] = useState(2);
     const [voteJSX, setVoteJSX] = useState(null);
 
@@ -16,85 +17,75 @@ export default function VoteView({idSession}) {
 
     // TODO : Gérer le cas où la décision a déjà été prise
 
-    useEffect (()=>{
-        function setViewVote(){
-            switch (voteState) {
-                case 0:
-                    //0:en attente de vote => Urne + boutons d'envoi/propose
-                    setVoteJSX(<ChoixUrne idSession={idSession} token={token} />); break;
-                case 1:
-                    //1:ne peut pas/plus voter => Urne visible
-                    setVoteJSX(<StaticUrne idSession={idSession} />); break;
-                case 2:
-                    //2:ne peut pas voir
-                    setVoteJSX(<Title label='AUCUN VOTE DISPONIBLE POUR L&apos;INSTANT'/>); break;
-            }
-        }
-    
-
+    useEffect(() => {
         /**
          * TODO : Fonction qui indique si on a accès aux votes
          */
-        async function canISeeVotes(){
+        function canISeeVotes() {
             let canSeeVote = false;
-            await fetch(`${BACKEND}/game/${idSession}/vote/check-see`, {
+            fetch(`${BACKEND}/game/${idSession}/vote/check-see`, {
                 method: 'GET',
-                headers: { 'x-access-token': token, 
-                'Content-Type': 'application/json' }
+                headers: {
+                    'x-access-token': token,
+                    'Content-Type': 'application/json'
+                }
             })
                 .then(response => response.json())
-                .then(data => {
-                    if (data.status == true){
-                        canSeeVote = true;
-                    } else {
-                        canSeeVote = false;
-                    }
-                });
-            return(canSeeVote);
+                .then(data => canSeeVote = data.status);
+            return canSeeVote;
         }
 
         /**
          * Fonction qui indique si j'ai le droit de voter
          */
-        async function canIVote(){
+        function canIVote() {
             let canVote = false;
-            await fetch(`${BACKEND}/game/${idSession}/vote/check`, {
+            fetch(`${BACKEND}/game/${idSession}/vote/check`, {
                 method: 'GET',
-                headers: { 'x-access-token': token, 
-                'Content-Type': 'application/json' }
+                headers: {
+                    'x-access-token': token,
+                    'Content-Type': 'application/json'
+                }
             })
                 .then(response => response.json())
-                .then(data => {
-                    if (data.status == true){
-                        canVote = true;
-                    } else {
-                       canVote = false;
-                    }
-                });
-            return(canVote);
+                .then(data => canVote = data.status);
+            return canVote;
         }
 
-        if (canISeeVotes()===true){ //! NE PAS ENLEVER LA COMPARAISON (sinon regarde si l'appel fonctionne seulement)
-            if (canIVote()===true){
-                console.log("Allowed to vote");
-                setVoteState(0);
+        if (currentGameView == gameViews.VOTE) {
+            if (canISeeVotes() === true) { //! NE PAS ENLEVER LA COMPARAISON (sinon regarde si l'appel fonctionne seulement)
+                if (canIVote() === true) {
+                    console.log('Allowed to vote');
+                    setVoteState(0);
+                } else {
+                    console.log('Not allowed to vote');
+                    setVoteState(1);
+                }
             } else {
-                console.log("Not allowed to vote");
-                setVoteState(1);
+                console.log('Not allowed to see the vote');
+                setVoteState(2);
             }
-        } else {
-            console.log("Not allowed to see the vote");
-            setVoteState(2);
         }
-        setViewVote();
 
-    },[currentGameView])
+    }, [currentGameView, idSession, token]);
 
+    useEffect(() => {
+        switch (voteState) {
+            case 0:
+                // 0: en attente de vote => Urne + boutons d'envoi/propose
+                setVoteJSX(<ChoixUrne idSession={idSession} token={token} />); break;
+            case 1:
+                // 1: ne peut pas/plus voter => Urne visible
+                setVoteJSX(<StaticUrne idSession={idSession} />); break;
+            case 2:
+                // 2:ne peut pas voir
+                setVoteJSX(<Title label='AUCUN VOTE DISPONIBLE POUR L&apos;INSTANT' />); break;
+        }
+    }, [idSession, token, voteState]);
 
     return (
-        <View style={styles.container}>
-            {voteJSX}
-        </View>);
+        <View style={styles.container}>{voteJSX}</View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -102,6 +93,5 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         flexDirection: 'column',
-      },
-    
-})
+    },
+});
